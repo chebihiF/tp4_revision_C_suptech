d36be9fbb6d67c6529bae60403e602716e922ec9
Chebihi <chebihi.infinitysoft@google.com>
2022-03-21 14:56:51 +0100

    add init methode

--- tp4_revision/main.c
+++ tp4_revision/main.c
@@ -8,13 +8,19 @@ typedef struct Cellule {
 	struct Cellule* suivant;
 } Cel;
 
+void init(Cel);
+
 void main() {
 	Cel emp1;
-	printf("Nom : ");
-	fgets(emp1.nom, 20, stdin);
-	printf("Email : ");
-	fgets(emp1.email, 30, stdin);
-	printf("salaire : ");
-	scanf_s("%lf", &emp1.salaire);
+	emp1.salaire = 0;
+	init(&emp1);
+}
 
+void init(Cel* emp1) {
+	printf("Nom : ");
+	fgets(emp1->nom, 20, stdin);
+	printf("Email : ");
+	fgets(emp1->email, 30, stdin);
+	printf("salaire : ");
+	scanf_s("%lf", &emp1->salaire);
 }
